8aca9da962481f0684b23f992d48a5175ce407bb
allen20306 <allenmanoj2006@gmail.com>
2026-07-30 22:53:10 +0530

    correction

--- assignments/assignment_week4/c/bitAnd.c
+++ assignments/assignment_week4/c/bitAnd.c
@@ -7,5 +7,5 @@ int bitAnd(int x, int y) {
 int main() {
     int x, y;
     scanf("%d %d", &x, &y);
-    printf("Custom operator: AND -> %d\n", bitAnd(6, 5));
+    printf("Custom operator: AND -> %d\n", bitAnd(x, y));
 }
